484f5983ce1760b169700c054f23ab563aef3fc5
Shuying Lin <shuyinglin47@gmail.com>
2017-11-05 12:35:05 +1100

    add color palette

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
+    <h1>Canvas</h1>
    <canvas id="canvas" v-on:mousedown="handleMouseDown" v-on:mouseup="handleMouseUp" v-on:mousemove="handleMouseMove" width="900px" height="600px"></canvas>
+   <div class="colorPlatte">
+     <ul>
+      <li v-for="color in colorPalette">
+        <div class="colorBlock" :style="{background: color}" v-on:mousedown="pickColor(color)"></div>
+        {{color}}
+      </li>
+     </ul>
+   </div>
   </div>
 </template>
 
@@ -19,7 +28,26 @@ export default {
           y: 0
         },
         down: false
-      }
+      },
+      colorPalette: {
+        white: "#fff",
+        silver: "#c0c0c0",
+        grey: "#808080",
+        black: "#000",
+        red: "#ff0000",
+        maroon: "#800000",
+        yellow: "#ffff00",
+        olive: "#808000",
+        lime: "#00ff00",
+        green: "#008000",
+        aqua: "#00ffff",
+        teal: "#008080",
+        blue: "#0000ff",
+        navy: "#000080",
+        fuchsia: "#ff00ff",
+        purple: "#800080" 
+      },
+      currentColor: "FFF"
     }
   },
   computed: {
@@ -34,13 +62,17 @@ export default {
     }
   },
   methods: {
+    pickColor: function(color) {
+      this.currentColor = color;
+    },
     draw: function (event) {
      if (this.mouse.down ) {
        var c = document.getElementById("canvas");
        var ctx = c.getContext("2d");
        ctx.clearRect(0,0,900,600);
        ctx.lineTo(this.currentMouse.x, this.currentMouse.y);
-       ctx.strokeStyle ="#FFF";
+       ctx.strokeStyle = this.currentColor;
+       console.log(ctx.strokeStyle);
        ctx.lineWidth = 2;
        ctx.stroke()
      } 
@@ -71,6 +103,7 @@ export default {
     var ctx = c.getContext("2d");
     ctx.translate(0.1, 0.1);
     ctx.imageSmoothingEnabled= false;
+    ctx.strokeStyle = "FFF";
   }
 }
 </script>
@@ -105,5 +138,11 @@ a {
 canvas{
   background:black;
   box-shadow: 0 2px 3px rgba(0,0,0,0.2)
+}
+.colorBlock{
+  width: 15px;
+  height: 15px;
+  border: 1px;
+  border-color: black;
 }
 </style>
